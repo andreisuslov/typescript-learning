f14410e59af933899c80280b0dd8433a1dd0ca51
Andrei Suslov <andrei.suslov@avid.com>
2022-04-25 14:17:18 -0400

    Add classes for app

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,1 +1,17 @@
-class User {}
+import faker from 'faker';
+
+export class User {
+    name: string;
+    location: { // an object with 2 properties
+        lat: number;
+        lng: number;
+    };
+
+    constructor() {
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()), // parseFloat() method converts string to number
+            lng: parseFloat(faker.address.longitude())
+        };
+    }
+}
