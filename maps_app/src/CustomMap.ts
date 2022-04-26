2faac34acd346c060fd376eb82942bfcfbc41ece
Andrei Suslov <andrei.suslov@avid.com>
2022-04-26 15:25:10 -0400

    Add markers to Maps App.

--- maps_app/src/CustomMap.ts
+++ maps_app/src/CustomMap.ts
@@ -1,26 +1,69 @@
-import { User } from './User';
-import { Company } from './Company';
+/// <reference types="@types/google.maps" />
+// import { User } from './User';
+// import { Company } from './Company';
+
+// Instructions to every other class on how they can ba an argumant to 'addMarker' method
+interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    };
+}
 
 export class CustomMap {
-    private googleMap: google.maps.Map; // property - variable referencing the instanse of google.maps.Map class
-    // private means anyone else who's using this classs cannot reach the instance and try to handle it on google map
+  private googleMap: google.maps.Map;
 
-    constructor(divId: string) {
-        // document.getElementById(divId) -> it is a reference to an html-element
-        this.googleMap = new google.maps.Map(document.getElementById(divId), {
-            zoom: 1,
-            center: {
-                lat: 0,
-                lng: 0
-            }
-        });
-    }
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
 
-    addUserMarker() {
-        
-    }
+//addUserMarker and addCompanyMarker methods are awfully similar to each other
 
-    addCompanyMarker() {
-        
-    }
+
+//   addUserMarker(user: User): void {
+//     new google.maps.Marker({
+//       map: this.googleMap,
+//       position: {
+//         lat: user.location.lat,
+//         lng: user.location.lng,
+//       },
+//     });
+//   }
+//   addCompanyMarker(company: Company): void {
+//     new google.maps.Marker({
+//         map: this.googleMap,
+//         position: {
+//           lat: company.location.lat,
+//           lng: company.location.lng,
+//         },
+//       });
+//   }
+
+// this is one possible solution. note: it's not great because it is not scalable and it introduces very tight coupling between 
+// addMarker(mappable: User | Company): void {
+//     new google.maps.Marker({
+//         map: this.googleMap,
+//         position: {
+//         lat: mappable.location.lat,
+//         lng: mappable.location.lng,
+//         },
+//     });
+// }
+
+// scalable solution of adding marker
+addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
 }
