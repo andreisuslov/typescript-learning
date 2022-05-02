59e3d1db3b148aefc0bff3cd95a2e2c97e1251ac
Andrei Suslov <truvord@gmail.com>
2022-05-02 16:11:46 -0400

    Add generics to Stats App

---
+++ stats_app/build/ScvFileReader.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CsvFileReader = void 0;
+class CsvFileReader {
+    constructor() {
+        this.data = [];
+    }
+}
+exports.CsvFileReader = CsvFileReader;
